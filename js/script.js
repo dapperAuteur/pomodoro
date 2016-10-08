c08ce53bca181bf22dc4bcfd3b383560edc12e9d
Anthony McDonald <info@elementarymba.com>
2016-10-07 20:02:08 -0700

    add comments to currentTime()

--- js/script.js
+++ js/script.js
@@ -25,17 +25,21 @@ function currentTime() {
             document.getElementById("newFocusTime").innerHTML = localStorage.getItem("timeObjStr");
             // Retrieve timeObjStr from localStorage and set it to HTML id newFocusTime
             document.getElementById("newRelaxTime").innerHTML = localStorage.getItem("timeObjStr");
-        } else {
+            //Retrieve newRelaxTime from HTML and set it to localStorage object "timeObjStr"
+        } else {//if Storage == "undefined"
             document.getElementById("newFocusTime").innerHTML = "Sorry, your browser does not support Web Storage...";
+            //Retrieve "newFocusTime" from HTML and set it to the string "Sorry, your browser does not support Web Storage..."
         };
     
     console.log(currentTime);
     console.log(currentTimeString);
     console.log(currentTimeSeconds);
-    //print 'currentTime' to console.log
+    //print 'currentTime', 'currentTimeString', 'currentTimeSeconds' to console.log
     document.getElementById('focus').innerHTML = currentTimeString;
+    //Retrieve 'focus' id from HTML and set it to currentTimeString
     //show current time
     return currentTime;
+    //return currentTime to function caller
 }
 
 //create variable called focusDuration and set its default value to 25 minutes
